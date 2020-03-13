3692a4b9301a5384de3990840b8c1f010a07bd9b
Анжелика Стрижич <ao.stryzhych@lab.vebtech.by>
2020-03-13 12:09:30 +0300

    Minimize code style

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // Лабораторная работа 2 по дисциплине ЛОИС
 // Выполнена студенткой группы 721702 БГУИР Стрижич Анжелика Олеговна
 // Файл содержит функции парсинга строки для проверки синтаксиса и подсчета значений подформул
-// 12.03.2020
+// 13.03.2020
 
 var checkingMessages = [
     "", // 0
@@ -20,41 +20,15 @@ var checkingMessages = [
 ];
 
 function checkSyntax(formula) {
-    if (!formula.match(/^([A-Z()|&!~10]|->)*$/g)) {
-        return 1;
-    }
-
-    if (!formula.match(/^\((\(*|[01A-Z]|\(!?[01A-Z]\))/) && !formula.match(/[A-Z01]/g)) {
-        return 2;
-    }
-
-    if (!formula.match(/[A-Z)01]\)$/) && !formula.match(/[A-Z01]/g)) {
-        return 3;
-    }
-
-    if (formula.match(/\)\(/)) {
-        return 4;
-    }
-
-    if (formula.match(/\(\)/)) {
-        return 5;
-    }
-
-    if (formula.match(/[A-Z]([^|&~]|(?!->))[A-Z]/) || formula.match(/(([|&~]|->)\))|\((([|&~]|->))/)) {
-        return 6;
-    }
-
-    if (formula.match(/[^(]![A-B]/) || formula.match(/![A-B][^)]/)) {
-        return 7;
-    }
-    
-    if (formula.match(/([|&~]|->)[A-Z]([|&~]|->)/g) || formula.match(/^[A-Z]([|&~]|->)[A-Z]$/g)) {
-        return 8;
-    }
-
-    if (formula.match(/\([A-Z]\)/)) {
-        return 9;
-    }
+    if (!formula.match(/^([A-Z()|&!~10]|->)*$/g)) return 1;
+    if (!formula.match(/^\((\(*|[01A-Z]|\(!?[01A-Z]\))/) && !formula.match(/[A-Z01]/g)) return 2;
+    if (!formula.match(/[A-Z)01]\)$/) && !formula.match(/[A-Z01]/g)) return 3;
+    if (formula.match(/\)\(/)) return 4;
+    if (formula.match(/\(\)/)) return 5;
+    if (formula.match(/[A-Z]([^|&~]|(?!->))[A-Z]/) || formula.match(/(([|&~]|->)\))|\((([|&~]|->))/)) return 6;
+    if (formula.match(/[^(]![A-B]/) || formula.match(/![A-B][^)]/)) return 7;
+    if (formula.match(/([|&~]|->)[A-Z]([|&~]|->)/g) || formula.match(/^[A-Z]([|&~]|->)[A-Z]$/g)) return 8;
+    if (formula.match(/\([A-Z]\)/)) return 9;
 
     return 0;
 }
@@ -63,31 +37,20 @@ function checkPairingBraces(formula) {
     let countOfOpenBraces = formula.split('(').length - 1;
     let countOfCloseBraces = formula.split(')').length - 1;
     
-    if (countOfOpenBraces > countOfCloseBraces) {
-        return 10;
-    }
-
-    if (countOfOpenBraces < countOfCloseBraces) {
-        return 11;
-    }
+    if (countOfOpenBraces > countOfCloseBraces) return 10;
+    if (countOfOpenBraces < countOfCloseBraces) return 11;
 
     return 0;
 }
 
 function checkFormula(formula) {
-    if (!formula) {
-        return 1;
-    }
+    if (!formula) return 1;
 
     let isSyntaxValid = checkSyntax(formula);
-    if (isSyntaxValid !== 0) {
-        return isSyntaxValid;
-    }
+    if (isSyntaxValid !== 0) return isSyntaxValid;
 
     let isBracesPaired = checkPairingBraces(formula);
-    if (isBracesPaired !== 0) {
-        return isBracesPaired;
-    }
+    if (isBracesPaired !== 0) return isBracesPaired;
 
     return 0;
 }
@@ -108,9 +71,7 @@ function build() {
     let resultElement = document.getElementById("result");
     let truthTableElement = document.getElementById("table");
 
-    if (formula.match(/^[A-Z]$/)) {
-        resultElement.innerHTML = formula;
-    }
+    if (formula.match(/^[A-Z]$/)) resultElement.innerHTML = formula;
 
     let atoms = getUniqueAtoms(formula);
     let valueSets = getValueSets(atoms);
@@ -133,39 +94,24 @@ function build() {
         if (functionResult === '1') {
             countOfGroups++;
 
-            if (valueSetNumber != valueSets.length - 1) {
-                pdnf += '(';
-            }
+            if (valueSetNumber != valueSets.length - 1) pdnf += '(';
 
             for (atomIndex = 0; atomIndex < atoms.length; atomIndex++) {
-                if (atomIndex != atoms.length - 1) {
-                    pdnf += '(';
-                }
+                if (atomIndex != atoms.length - 1) pdnf += '(';
                 
-                if (valueSets[valueSetNumber][atomIndex] === '0') {
-                    pdnf += '(!' + atoms[atomIndex] + ')';
-                } else {
-                    pdnf += atoms[atomIndex];
-                }
+                if (valueSets[valueSetNumber][atomIndex] === '0') pdnf += '(!' + atoms[atomIndex] + ')';
+                else pdnf += atoms[atomIndex];
 
-                if (atomIndex != atoms.length - 1) {
-                    pdnf += '&';
-                }
+                if (atomIndex != atoms.length - 1) pdnf += '&';
             }
 
-            for (atomIndex = 0; atomIndex < atoms.length - 1; atomIndex++) {
-                pdnf += ')';
-            }
+            for (atomIndex = 0; atomIndex < atoms.length - 1; atomIndex++) pdnf += ')';
 
-            if (valueSetNumber != valueSets.length - 1) {
-                pdnf += '|';
-            }
+            if (valueSetNumber != valueSets.length - 1) pdnf += '|';
         }
     }
 
-    for (i = 0; i < countOfGroups - 1; i++) {
-        pdnf += ')';
-    }
+    for (i = 0; i < countOfGroups - 1; i++) pdnf += ')';
 
     messageText.innerHTML = '♥';
     resultElement.innerHTML = pdnf;
@@ -178,6 +124,7 @@ function getUniqueAtoms(formula) {
 
 function getValueSets(atoms) {
     let sets = [];
+
     for (let row = 0; row < Math.pow(2, atoms.length); row++) {
         sets.push([]);
         let binaryNumber = Array.from(row.toString(2));
@@ -187,9 +134,7 @@ function getValueSets(atoms) {
             binaryNumber.forEach(digit => {
                 sets[row].push(digit);                
             });
-        } else {
-            sets[row] = binaryNumber;
-        }
+        } else sets[row] = binaryNumber;
     }
     
     return sets;
@@ -214,12 +159,4 @@ function calculateFunctionResult(formulaWithValues) {
     }
 
     return formulaWithValues;
-}
-
-class TruthTable {
-    constructor(atoms, values, functionResults) {
-        this.atoms = atoms;
-        this.values = values;
-        this.functionResults = functionResults;
-    }
 }
